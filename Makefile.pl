5e325974a4bb8dd8cd9850b2a768a5b576a6b9f3
Z. Bornheimer <z@chary.me>
2018-07-01 12:51:40 -0400

    Makefile.pl generates the modular "deploy" script and associated zip file.

--- Makefile.pl
+++ Makefile.pl
@@ -5,29 +5,62 @@
 use strict;
 use warnings;
 use Carp;
+use Path::Tiny qw( path );
+use File::Compare;
+use File::Copy;
 
-my @files = qw| ZysysHosting.php |;
+# Tasks:
+#   1) identify the version information stored in VERSION and apply it to the script, subbing the contents of the VERSION file with the {VERSION}
+#   2) identify all the require(), require_once(), and includes() and include them via concatenation...note: remove starting <?php and ?> from file.
 
-open (ZY, 'ZysysHosting.php');
-my $version = 0;
-while (<ZY>) {
-    if (/Version: ([\d\.]+)/) {
-        $version = $1;
-    }
+my $orig_staging = 'ZysysHosting.php';
+my $version_file = "VERSION";
+my $deploy_script = "ZysysHosting-deploy.php";
 
-    if (/define\('ZYSYSHOSTING_OPTIMIZATIONS_VERSION', '([\d\.]+)'/) {
-        if ($version ne $1) {
-            croak "ERROR: Hey, your versions aren't matching up.  Please confirm you've updated the plugin version and the constants.\n\t";
+
+
+chomp(my $version = slurp($version_file));
+my $tmp_file_id = 0; #temp 
+
+my $staging = $orig_staging;
+my $repeat = 1;
+while ($repeat == 1) {
+    open (ZY, $staging);
+    open (ZYOUT, ">.$deploy_script.makezytmp$tmp_file_id");
+    while (<ZY>) {
+        my $line = $_;
+        if (/(require|include)(_once){0,1}\(['"](.*)["']\);/) {
+            $line = slurp($3);
+            $line =~ s/^<\?(php)*//;
+            $line =~ s/\?>$//;
         }
+        $line =~ s/{VERSION}/$version/g;
+        print ZYOUT $line;
+    }
+    close (ZY);
+    close(ZYOUT);
+    if (compare($staging, ".$deploy_script.makezytmp$tmp_file_id") != 0) {
+        $staging = ".$deploy_script.makezytmp$tmp_file_id";
+        $tmp_file_id++;
+    } else {
+        $repeat = 0;
     }
 }
-close (ZY);
+
+move(".$deploy_script.makezytmp$tmp_file_id", $deploy_script);
+system("rm .$deploy_script.makezytmp*");
 
 if (`mkdir ZysysHostingOptimizations 2>&1 3>&1` =~ /exists/) {
     croak "ERROR: The folder ZysysHostingOptimizations exists. Please remove it.\n\t";
 }
-foreach (@files) {
-    system("cp -r $_ ZysysHostingOptimizations");
-}
+system("cp -r $deploy_script ZysysHostingOptimizations");
 system ("zip -r ZysysHostingOptimizations-" . $version . ".zip ZysysHostingOptimizations/");
 system("rm -R ZysysHostingOptimizations/");
+
+
+# https://perlmaven.com/slurp
+sub slurp {
+    my $file = shift;
+    return path($file)->slurp_utf8;
+}
+
